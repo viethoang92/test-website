0633f61063bd58bd332503a3b80b4b685e802cbe
Wyatt92 <vi_hoang@hotmail.de>
2017-08-17 23:01:46 +0200

    structure

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-    $(".content").load("content.html");
+    $("#sectionI").load("html/sectionI.html");
+    $("#sectionII").load("html/sectionII.html");
+    
+    sectionToggle();
 });
 
+/* onClick, show subSections*/
+function sectionToggle() {
+    $(".subSection").hide();
+    $(".section").on('click', function() {
+        $(this).toggleClass('active');
+        $(this).next().slideToggle(100);
+    })
+}
 
-$(document).on('click', 'a', function (event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+/* onClick, jump to Section */
+$(document).on('click', 'a', function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 'fast');
+        return this; // for chaining...
+    }
 });
