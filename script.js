14277640b8c70b8fcbf411388ed81e97f1dd6a18
Wyatt92 <vi_hoang@hotmail.de>
2017-08-17 21:37:10 +0200

    structure

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+$(document).ready(function () {
+    $(".content").load("content.html");
+});
 
-    $(document).on('click', 'a', function (event) {
-        event.preventDefault();
 
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
-    });
+$(document).on('click', 'a', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
