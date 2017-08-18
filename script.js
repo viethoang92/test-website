8e7739f13f64c04671ccb72237144ff441db85f6
Wyatt92 <vi_hoang@hotmail.de>
2017-08-18 19:08:50 +0200

    gimmicks

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 $(document).ready(function () {
     $("#sectionI").load("html/sectionI.html");
     $("#sectionII").load("html/sectionII.html");
-    
+    $("#sectionIII").load("html/sectionIII.html");
+    sectionJump();
     sectionToggle();
+    highlighted();
+
 });
+
+/* onClick: highlight  */
+function highlighted() {
+    $('.sideNav a').hover(
+    function(){
+        $(this).addClass("highlighted");
+    
+    },
+    function(){
+        $(this).removeClass("highlighted");
+    }
+  );
+
+}
+
 
 /* onClick, show subSections*/
 function sectionToggle() {
     $(".subSection").hide();
-    $(".section").on('click', function() {
+    $(".section").on('click', function () {
         $(this).toggleClass('active');
         $(this).next().slideToggle(100);
     })
 }
 
 /* onClick, jump to Section */
-$(document).on('click', 'a', function($) {
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top + 'px'
-        }, 'fast');
-        return this; // for chaining...
-    }
-});
+function sectionJump() {
+    $(document).on('click', 'a', function ($) {
+        $.fn.goTo = function () {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top + 'px'
+            }, 'fast');
+            return this; // for chaining...
+        }
+    });
+}
